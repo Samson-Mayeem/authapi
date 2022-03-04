2c7bf91ac0cea8da054c31e94a2a0e3428dd8817
Samson-Mayeem <63527034+Samson-Mayeem@users.noreply.github.com>
2022-03-04 05:31:15 +0000

    from this level there a no-MySql rethrow error

--- login.js
+++ login.js
@@ -2,15 +2,18 @@ const mysql = require("mysql")
 const express = require("express")
 const { use } = require("express/lib/router")
 const app = express()
+const bodyparser = require("body-parser")
+var encoder = bodyparser.urlencoded();
+
 app.use('/assets', express.static('assets'))
-const connections = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'hello',
     database: 'logintest_db'
 });
 
-connections.connect(function(err){
+connection.connect(function(err){
     if (err)  throw err;
     else { 
         console.log("Connection is successfully established");
@@ -20,15 +23,21 @@ connections.connect(function(err){
 app.listen(4500);
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + './')
+    res.sendFile(__dirname + "/login.html");
 })
-app.post('/', function(req, res){
-    connections.query("select * from user where user_name = ? and password = ?", function(err, results, fields){
+app.post('/', encoder, function(req, res){
+    var email = req.body.email;
+    var password = req.body.password;
+    connection.query("select * from user where email = ? and password = ?", [email, password], function(err, results, fields){
         if(results.length > 0){
-            res.redirect('/homepage.html');
+            res.redirect('/home');
         }
         else{
-
+            res.redirect("/");
         }
+        res.end();
     });
+})
+app.get('/home', function(req, res){
+    res.sendFile(__dirname + "/home.html")
 })
